3432c01278a7a4096b7063ddf1ca8a010110a0c9
cho1490 <chosanghyun99@gmail.com>
2020-07-23 19:30:09 +0900

    Fix FragmentBest efficiently

--- app/src/main/java/sjsh/com/FragmentBest.java
+++ app/src/main/java/sjsh/com/FragmentBest.java
@@ -20,8 +20,6 @@ import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.RequestConfiguration;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
